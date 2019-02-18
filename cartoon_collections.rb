a3d623e0c23133df7227ffdd5e15c3b64121c91f
Ryan Pethel <arpethel@gmail.com>
2019-02-18 17:24:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,9 @@ def roll_call_dwarves(array)
   end
 end
 
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
+def summon_captain_planet(array)
+  array.collect { |ele| ele.capitalize + "!" }
+end
 #
 # def long_planeteer_calls# code an argument here
 #   # Your code here
@@ -18,11 +18,7 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-def summon_captain_planet(array)
-  array.collect do |ele|
-    ele.capitalize + "!"
-  end
-end
+
 
 def long_planeteer_calls(array)
   array.any? { |name| name.length > 4 }
