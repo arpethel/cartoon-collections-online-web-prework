81e79aefb279f91bc74c1eca9aa04dbd94d37f2e
Ryan Pethel <arpethel@gmail.com>
2019-02-18 17:38:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,12 +11,5 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  arr = array.join(" ")
-  arr.find do |item|
-    item.include?(cheese_types)
-    return item
-  end
-  # return item
+  
 end
