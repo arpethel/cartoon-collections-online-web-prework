1840bcde51885fe2ec6f23ab687957ae6d2c04bc
Ryan Pethel <arpethel@gmail.com>
2019-02-18 17:21:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,7 @@ def find_the_cheese# code an argument here
 end
 
 def summon_captain_planet(array)
-  array.collect do |ele|
-    ele.capitalize + "!"
-  end
+  array.collect { |ele| ele.capitalize + "!" }
 end
 
 def long_planeteer_calls(array)
